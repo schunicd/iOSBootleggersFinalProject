93961a70a3c230816389e9d979195ec83bda81a1
Lucas Zanlorensi <lucas_zanlorensi@hotmail.com>
2021-04-18 20:53:06 -0400

    Fixed keyboard not going away and alert being called twice

--- iOSBootleggersFinalProject/iOSBootleggersFinalProject/ViewControllers/CustomerSignUpViewController.swift
+++ iOSBootleggersFinalProject/iOSBootleggersFinalProject/ViewControllers/CustomerSignUpViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class CustomerSignUpViewController : UIViewController {
+class CustomerSignUpViewController : UIViewController, UITextFieldDelegate {
     
     @IBOutlet var firstNameTxt: UITextField!
     @IBOutlet var lastNameTxt: UITextField!
@@ -16,28 +16,34 @@ class CustomerSignUpViewController : UIViewController {
     let appDelegate = UIApplication.shared.delegate as! AppDelegate
     
     @IBAction func submitInfoToNewsletter(){
-        let alert: UIAlertController
+        let alert1: UIAlertController
+        let alert2: UIAlertController
         
         if firstNameTxt.text!.isEmpty || lastNameTxt.text!.count == 0 || emailTxt.text!.count == 0{
-            alert = UIAlertController(title: "Required", message: "Please fill in all the information", preferredStyle: .alert)
-            alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
-            self.present(alert, animated: true, completion: nil)
+            alert1 = UIAlertController(title: "Required", message: "Please fill in all the information", preferredStyle: .alert)
+            alert1.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+            self.present(alert1, animated: true)
         }
         else{
-            alert = UIAlertController(title: "Success", message: "Thank you for signing up for the newsletter!", preferredStyle: .alert)
-            alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
-            self.present(alert, animated: true, completion: nil)
-            
+            alert2 = UIAlertController(title: "Success", message: "Thank you for signing up for the newsletter!", preferredStyle: .alert)
+            alert2.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
             
             let myData: Data = Data()
             myData.initWithData(theRow: 1, theFirstName: firstNameTxt.text!, theEmail: emailTxt.text!, theLastName: lastNameTxt.text!)
             if appDelegate.insertIntoDatabase(entry: myData){
-                self.present(alert, animated: true, completion: nil)
+                self.present(alert2, animated: true, completion: nil)
             }
         }
     }
     
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+    }
+    
     override func viewDidLoad() {
-        
+        super.viewDidLoad()
+        lastNameTxt.delegate = self
+        firstNameTxt.delegate = self
+        emailTxt.delegate = self
     }
 }
